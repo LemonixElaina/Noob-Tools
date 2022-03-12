9ee00a343469cf50e10183e047bcfa4658beeaf1
Lemonix-xxx <lemonixlengyi@foxmail.com>
2022-03-12 16:50:51 +0800

    update the main feature

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Noob-Tools
-Some useful tools for each noob coder!
+Some useful tools for each noob Javascript coder!
+
+## Description
+*version*: 0.0.1a
+
+## Note
+The **Javascript** venv must be equal or greater than _ES6+_.
---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Package: NoobTools
+ * Author: Lemonix
+ * Page: github.com/lemonix-xxx
+ */
+
+import str from './lib/str.js'
+import func from './lib/func.js'
+import arr from './lib/arr.js'
+import obj from './lib/obj.js'
+import num from './lib/num.js'
+import bool from './lib/bool.js'
+
+export {
+    str,
+    func,
+    arr
+}
